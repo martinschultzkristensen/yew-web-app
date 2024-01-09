3b9f9a68e914837cc9373e4a05c81f7be1e5cdb5
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-01-09 22:18:38 +0100

    deleted commented old code in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -20,36 +20,7 @@ pub fn app() -> Html {
     
 
     // Handle keydown events to switch videos
-    // let handle_keydown_toggle = {
-    //     let videos = videos.clone();
-    //     let current_video_index = current_video_index.clone();
-    //     Callback::from(move |event: KeyboardEvent| {
-    //         if event.key() == "w" || event.key() == "s" {
-    //             let new_index = match event.key().as_str() {
-    //                 "w" => {
-    //                     let next_index = *current_video_index + 1;
-    //                     if next_index >= videos.len() {
-    //                         0
-    //                     } else {
-    //                         next_index
-    //                     }
-    //                 }
-    //                 "s" => {
-    //                     let prev_index = *current_video_index as i32 - 1;
-    //                     if prev_index < 0 {
-    //                         (videos.len() - 1) as usize
-    //                     } else {
-    //                         prev_index as usize
-    //                     }
-    //                 }
-    //                 _ => *current_video_index,
-    //             };
-    //             current_video_index.set(new_index);
-    //             let audio = web_sys::HtmlAudioElement::new_with_src("static/button-124476.mp3").unwrap();
-    //             let _ = audio.play();
-    //         }
-    //     })
-    // };
+
     let handle_keydown_toggle = get_toggle_key(videos, current_video_index);
 
     html! {        
