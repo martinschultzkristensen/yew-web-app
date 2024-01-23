4df99ec22ecaf50cd14484148bb541ba70db0b32
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-01-23 16:50:31 +0100

    Added file intro_screen and main_menu in organismsfolder. On the way to implement route

---
+++ src/components/organisms/main_menu.rs
@@ -0,0 +1,20 @@
+use yew::prelude;
+
+#[function_component(MainMenu)]
+pub fn main_menu() -> Html {
+    
+    let demo_videos = get_demo_videos();
+    // State to track the index of the currently displayed demo video
+    let current_video_index = use_state(|| 0);
+
+    //Handle keydown events to switch videos
+    let handle_keydown_toggle = get_toggle_key(&demo_videos, current_video_index.clone());
+
+    html! {
+               
+        <div onkeydown={handle_keydown_toggle} tabindex="0">
+            <VideosList videos={demo_videos} current_index={*current_video_index} />
+            <img src="static/danceOmatic_logo.png" alt="logo of danceomatic"/>
+        </div>
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -10,27 +10,26 @@ use components::organisms::keydown_logic::get_toggle_key;
 
 mod components;
 
+#[derive(Clone, Switch, Debug)]
+pub enum AppRoute {
+    #[to = "/main-menu"]
+    MainMenu,
+    #[to = "/intro-screen"]
+    IntroScreen,
+}
 
 
 #[function_component(DanceOmatic)]
 pub fn app() -> Html {
-    // let intro_video = get_intro_video();
-    let demo_videos = get_demo_videos();
-    // State to track the index of the currently displayed demo video
-    let current_video_index = use_state(|| 0);
-    
-    
-
-    // Handle keydown events to switch videos
-
-    let handle_keydown_toggle = get_toggle_key(&demo_videos, current_video_index.clone());
-
-        //<IntroVideo videos=intro_video> <-- I want this to be implementet here
-    html! {        
-        <div onkeydown={handle_keydown_toggle} tabindex="0">
-            <VideosList videos={demo_videos} current_index={*current_video_index} />
-            //<img src="static/danceOmatic_logo.png" alt="logo of danceomatic"/>
-        </div>
+    html! {
+        <Router<AppRoute, ()>
+            render = Router::render(|switch: AppRoute| {
+                match switch {
+                    AppRoute::MainMenu => html! { <main_menu::MainMenu /> },
+                    AppRoute::VideoList => html! { <video_list::VideoList /> },
+                }
+            })
+        />
     }
 }
 
