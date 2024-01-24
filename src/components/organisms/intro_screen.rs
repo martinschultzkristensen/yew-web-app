3f8612839aa8561e0235dcb31d5997177a7d728a
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-01-24 21:35:50 +0100

    wokring on fn exit_video in keydown_logic.rs

--- src/components/organisms/intro_screen.rs
+++ src/components/organisms/intro_screen.rs
@@ -9,8 +9,13 @@ pub fn intro_screen() -> Html {
     let current_video_index = use_state(|| 0);
     let x_to_exit = exit_video(&intro_video, current_video_index.clone());
     html! {
+    <div>
+        <h1> {"Hello"} </h1>
+        
         <div onkeydown={x_to_exit} tabindex="0">
+            <h1> {"HEYff"} </h1>
             <VideosList videos={intro_video} current_index={*current_video_index}/>
         </div>
+    </div>
     }
 }
