cf644c2118e3858857254c0db3dd31be77c3a2b3
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-02-16 18:55:59 +0100

    new pub fn choreo_videos in video_data.rs

--- src/components/data/video_data.rs
+++ src/components/data/video_data.rs
@@ -34,3 +34,28 @@ pub fn get_intro_video() -> Vec<Video> {
         },
     ]
 }
+
+pub fn choreo_videos() -> Vec<Video> {
+    vec![
+        Video {
+            id: 1,
+            title: "dancevideo nr.1".to_string(),
+            url: "static/LetsDuet_中文_countdown.mp4".to_string(),
+        },
+        Video {
+            id: 2,
+            title: "dancevideo nr.2".to_string(),
+            url: "static/siblings_中文_countdown.mp4".to_string(),
+        },
+        Video {
+            id: 3,
+            title: "dancevideo nr.3".to_string(),
+            url: "static/culture4Fun_中文_countdown.mp4".to_string(),
+        },
+        Video {
+            id: 4,
+            title: "dancevideo nr.4".to_string(),
+            url: "static/HejNihao_中文_countdown.mp4".to_string(),
+        },
+    ]
+}
