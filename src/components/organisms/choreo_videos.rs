404500d9ac09930fc4776d049a54e9edcb90c393
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-02-18 14:27:45 +0100

    press "e" in AboutChoreo1 to get choreo_video1

--- src/components/organisms/choreo_videos.rs
+++ src/components/organisms/choreo_videos.rs
@@ -1,8 +1,26 @@
 use yew::prelude::*;
-use crate::{choreo_videos, Route};
+use crate::VideosList;
+use crate::choreo_videos;
 use yew_router::prelude::use_navigator;
+use crate::Route;
 
 
-// #[function_component(ChoreoVideo)]
-// pub fn choreo_videos() -> Html {
-// }
+#[function_component(ChoreoVideo)]
+pub fn choreographic_videos() -> Html { //<-- change to performance video
+    let navigator = use_navigator().unwrap();
+    let performance_video = choreo_videos();
+    let performance_video_index = use_state(|| 0);
+
+    let restart_app = Callback::from(move |event: KeyboardEvent| {
+        if event.key() == "q" {
+            navigator.push(&Route::IntroScreen1);
+        }
+    });
+
+
+    html! {
+        <div onkeydown={restart_app} tabindex="0">
+            <VideosList videos={performance_video} current_index={*performance_video_index} />
+        </div>
+    }
+}
