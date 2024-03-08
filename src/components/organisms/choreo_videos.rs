6d435e2d791ed46c5912a8f1614a71b6285d5890
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-03-08 23:08:56 +0100

    compiling, added fnComponent VideosListNoLoop added to choreovideo()

--- src/components/organisms/choreo_videos.rs
+++ src/components/organisms/choreo_videos.rs
@@ -1,5 +1,6 @@
 use yew::prelude::*;
 use crate::choreo_videos;
+use crate::components::molecules::video_list::VideosListNoLoop;
 use yew_router::prelude::{use_navigator, Navigator};
 use crate::use_location;
 use crate::Route;
@@ -12,27 +13,27 @@ use std::borrow::Borrow;
 
 #[function_component(ChoreoVideo)]
 pub fn choreographic_videos() -> Html { //<-- change to performance video
-    let showdown_videos = choreo_videos(); 
-    let showdown_video_index: usize = use_location()
+    let choreo_videos = choreo_videos(); 
+    let choreo_video_index: usize = use_location()
         .and_then(|l| l.state::<usize>().map(|i| *i))
         .unwrap_or(0);
 
-    let showdown_video_index: UseStateHandle<usize> = use_state(|| showdown_video_index);
-
+    let choreo_video_index: UseStateHandle<usize> = use_state(|| choreo_video_index);
+    let navigator = use_navigator().unwrap();
      // Use state to track whether the video has finished playing
-     let has_video_finished = use_state(|| false);
+    //  let has_video_finished = use_state(|| false);
 
      // Callback for handling the ended event of the video element
-     let handle_video_ended = {
-        let navigator = navigator.clone();
-        let has_video_finished = has_video_finished.clone();
-         Callback::from(move |_| {
-             // If the video has finished playing, navigate to the load video action
-             if *has_video_finished.get() {
-                 navigator.push(&loadscreen_video());
-             }
-         })
-     };
+    //  let handle_video_ended = {
+    //     let navigator = navigator.clone();
+    //     let has_video_finished = has_video_finished.clone();
+    //      Callback::from(move |_| {
+    //          // If the video has finished playing, navigate to the load video action
+    //          if *has_video_finished.get() {
+    //              navigator.push(&loadscreen_video());
+    //          }
+    //      })
+    //  };
 
 
     let restart_app = Callback::from(move |event: KeyboardEvent| {
@@ -43,7 +44,7 @@ pub fn choreographic_videos() -> Html { //<-- change to performance video
 
     html! {
         <div onkeydown={restart_app} tabindex="0">
-            <VideosList videos={showdown_videos} current_index={*showdown_video_index}/>
+            <VideosListNoLoop videos={choreo_videos} current_index={*choreo_video_index}/>
         </div>
     }
 }
