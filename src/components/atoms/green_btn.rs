a062121b5a38678862d3408dee26ea775d22acf9
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-10-25 08:36:41 +0200

    compiling code, with attempt of controling greenBtn.svg timing from within #[function_component(BtnExplainerGraphics)]
    However not reacting.

--- src/components/atoms/green_btn.rs
+++ src/components/atoms/green_btn.rs
@@ -1,11 +1,27 @@
+//src/components/atoms/green_btn.rs
+//Delete this file
 
+use web_sys::Element;
+use yew::prelude::*;
 
-#[derive(Properties, PartialEq)]
-pub struct 3D_btn {
-  #[prop_or_default]
-  pub class: String,
-}
-
-#[function_component(3D_btn)]
-pub fn dance_o_matic_logo(props: &DanceOMaticLogoProps) -> Html {
+#[function_component(GreenButton)]
+pub fn green_button() -> Html {
     let svg_ref = use_node_ref();
+
+    // Add animation effect when component mounts
+    use_effect({
+        let svg_ref = svg_ref.clone();
+        move || {
+            if let Some(svg_element) = svg_ref.cast::<Element>() {
+                if let Ok(Some(circle)) = svg_element.query_selector("#smCircle") {
+                    let _ = circle.set_attribute("class", "green-btn-animate");
+                }
+            }
+            || ()
+        }
+    });
+
+    html! {
+    
+}}
+          
