627810b73fca250e21ec02129197d13d375ad3de
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-11-02 16:25:35 +0100

    statelocation is now fixed.

--- src/components/organisms/mod.rs
+++ src/components/organisms/mod.rs
@@ -1,6 +1,6 @@
 //components/organisms/mod.rs
 //without this file, the other files inside this folder cannot be called by lib.rs
-pub mod about_choreo1;
+pub mod about_choreo;
 pub mod choreo_videos;
 pub mod intro_screen;
 pub mod load_screen;
