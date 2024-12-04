11d0b7590c6189e6a1bb0c4472d5bd589dd22708
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-12-04 19:18:32 +0100

    dublicate arrow animation arrow up.

--- src/components/atoms/arrow_respnd_ui.rs
+++ src/components/atoms/arrow_respnd_ui.rs
@@ -46,11 +46,13 @@ pub fn arrow_up_icon(props: &ArrowIconProps) -> Html {
     });
 
     let bounce_class = if *respond { "arrow-small" } else { "" };
+    let outline_class = if *respond { "arrow-outline" } else { "" };
 
     html! {
+    <div>
         <svg 
             class={classes!(props.class.clone(), "gradient-icon", bounce_class)}
-            style={format!("transform: {}", props.transform)}
+            style="position: absolute;"
             xmlns="http://www.w3.org/2000/svg" 
             width="24"
             height="24"
@@ -64,6 +66,24 @@ pub fn arrow_up_icon(props: &ArrowIconProps) -> Html {
             <line x1="12" y1="5" x2="12" y2="19"></line>
             <polyline points="5 12 12 5 19 12"></polyline> // "Up" SVG points
         </svg>
+        // Outlined Duplicate
+            <svg 
+            class={classes!(props.class.clone(), "gradient-icon", outline_class)}
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24"
+                height="24"
+                viewBox="0 0 24 24" 
+                fill="pink" 
+                stroke="white" 
+                stroke-width="2" 
+                stroke-linecap="round" 
+                stroke-linejoin="round"
+            >
+                <line x1="12" y1="5" x2="12" y2="19"></line>
+                <polyline points="5 12 12 5 19 12"></polyline>
+            </svg>
+        </div>
+        
     }
 }
 
