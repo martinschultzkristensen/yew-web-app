3fafb955ddfa904e0ee68cd61bf0499c823230d9
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-12-07 20:34:23 +0100

    arrow up and down graphical implementation with effect in main menu

--- src/components/atoms/arrow_respnd_ui.rs
+++ src/components/atoms/arrow_respnd_ui.rs
@@ -10,38 +10,7 @@ pub struct ArrowIconProps {
     pub transform: String,
 }
 
- // <svg 
-        //     class={classes!(props.class.clone(), bounce_class)}
-        //     style="position: absolute;"
-        //     xmlns="http://www.w3.org/2000/svg" 
-        //     width="24"
-        //     height="24"
-        //     viewBox="0 0 24 24" 
-        //     fill="white" 
-        //     stroke="white" 
-        //     stroke-width="2" 
-        //     stroke-linecap="round" 
-        //     stroke-linejoin="round"
-        // >
-        //     <line x1="12" y1="5" x2="12" y2="19"></line>
-        //     <polyline points="5 12 12 5 19 12"></polyline> // "Up" SVG points
-        // </svg>
-        // Outlined Duplicate
-            // <svg 
-            // class={classes!(props.class.clone(), outline_class)}
-            //     xmlns="http://www.w3.org/2000/svg" 
-            //     width="24"
-            //     height="24"
-            //     viewBox="0 0 24 24" 
-            //     fill="pink" 
-            //     stroke="white" 
-            //     stroke-width="2" 
-            //     stroke-linecap="round" 
-            //     stroke-linejoin="round"
-            // >
-            //     <line x1="12" y1="5" x2="12" y2="19"></line>
-            //     <polyline points="5 12 12 5 19 12"></polyline>
-            // </svg>
+
 
 
 #[function_component(ArrowUpIcon)]
@@ -91,7 +60,7 @@ pub fn arrow_up_icon(props: &ArrowIconProps) -> Html {
 
 
 #[function_component(ArrowDownIcon)]
-pub fn arrow_down_icon() -> Html {
+pub fn arrow_down_icon(props: &ArrowIconProps) -> Html {
     let respond = use_state(|| false);
 
     let respond_clone = respond.clone();
@@ -123,24 +92,14 @@ pub fn arrow_down_icon() -> Html {
         }
     });
 
-    let bounce_class = if *respond { "bounce" } else { "" };
+    let bounce_class = if *respond { "arrow-small" } else { "" };
+    let outline_class = if *respond { "arrow-outline" } else { "" };
 
     html! {
-        <svg 
-            class={classes!(bounce_class)}
-            xmlns="http://www.w3.org/2000/svg" 
-            width="24"
-            height="24"
-            viewBox="0 0 24 24" 
-            fill="white" 
-            stroke="white" 
-            stroke-width="2" 
-            stroke-linecap="round" 
-            stroke-linejoin="round"
-        >
-            <line x1="12" y1="5" x2="12" y2="19"></line>
-            <polyline points="5 12 12 19 19 12"></polyline> // "Down" SVG points
-        </svg>
+        <div class="arrow-stack">
+                <object type="image/svg+xml" data="/static/arrowDownPixel.svg" class={classes!(props.class.clone(), bounce_class, "base-arrow")}></object>
+                <object type="image/svg+xml" data="/static/pinkArrowDown.svg" class={classes!(props.class.clone(), outline_class)}></object>
+        </div>
     }
 }
 
