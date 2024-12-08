87faaa8a90c77c834930871e17e1b0ea77541bff
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-12-08 11:22:54 +0100

    removed leftover code from old arrow component test

--- src/components/molecules/keydown_logic.rs
+++ src/components/molecules/keydown_logic.rs
@@ -1,13 +1,10 @@
 use crate::components::molecules::video_list::VideoType;
-use web_sys::HtmlElement;
-use yew::NodeRef;
 use yew::prelude::*;
 
 // Handle keydown events to switch demo videos in the main menu
 pub fn get_toggle_key(
     v: &Vec<VideoType>,
     video_index: UseStateHandle<usize>,
-        arrow_size: UseStateHandle<u32>,
 ) -> Callback<KeyboardEvent> {
     
     let videos = v.clone();
@@ -20,7 +17,6 @@ pub fn get_toggle_key(
             let current_index = current_video_index.clone();
             let new_index = match event.key().as_str() {
                 "w" => {
-                    arrow_size.set((*arrow_size + 5).min(100));
                     let next_index = *current_index + 1;
                     if next_index >= videos.len() {
                         0
@@ -29,7 +25,6 @@ pub fn get_toggle_key(
                     }
                 }
                 "s" => {
-                    arrow_size.set((*arrow_size - 5).max(10));
                     let prev_index = *current_index as i32 - 1;
                     if prev_index < 0 {
                         (videos.len() - 1) as usize
