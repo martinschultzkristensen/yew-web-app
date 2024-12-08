5c37166207917e2e6544001e295be5b68230cb1f
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-12-08 11:42:04 +0100

    audio in succesfully implemented in scollable_div.rs

--- src/components/molecules/scollable_div.rs
+++ src/components/molecules/scollable_div.rs
@@ -18,6 +18,11 @@ pub fn scrollable_div(props: &ScrollableDivProps) -> Html {
     // Clone props.onkeydown to avoid lifetime issues in the Callback closure
     let parent_onkeydown = props.onkeydown.clone();
 
+    // Audio element for playback
+    let audio = use_state(|| {
+        web_sys::HtmlAudioElement::new_with_src("/static/button-124476.mp3").unwrap()
+    });
+
     // Focus the div when the component mounts
     use_effect(move || {
         if let Some(div) = div_ref_clone.cast::<HtmlElement>() {
@@ -26,17 +31,23 @@ pub fn scrollable_div(props: &ScrollableDivProps) -> Html {
         || ()
     });
 
+    
     // Keydown handler for "W" and "S" keys to scroll
     let onkeydown = {
         let div_ref = div_ref.clone();
+        let audio = audio.clone();
         Callback::from(move |event: KeyboardEvent| {
             if let Some(div) = div_ref.cast::<HtmlElement>() {
                 match event.key().as_str() {
                     "w" => {
+                        audio.set_current_time(0.0);
+                        let _ = audio.play();
                         // Scroll up by 50px when "W" is pressed
                         div.scroll_by_with_x_and_y(0.0, -50.0);
                     }
                     "s" => {
+                        audio.set_current_time(0.0);
+                        let _ = audio.play();
                         // Scroll down by 50px when "S" is pressed
                         div.scroll_by_with_x_and_y(0.0, 50.0);
                     }
