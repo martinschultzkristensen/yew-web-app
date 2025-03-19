53bb60bfd906c8ffe07ac850c7a0f4a71c7744c5
martinschultzkristensen <m4rtinsk@protonmail.com>
2025-03-19 21:58:21 +0100

    compiling. from config.toml choreo_videos not proberly impl.

--- src/components/data/video_data.rs
+++ src/components/data/video_data.rs
@@ -1,5 +1,6 @@
 //src/components/data/video_data.rs
 use crate::components::molecules::video_list::{Video, VideoType};
+use crate::components::data::config::Config;
 
 pub fn get_intro_video() -> Vec<VideoType> {
     vec![
@@ -10,30 +11,33 @@ pub fn get_intro_video() -> Vec<VideoType> {
     })]
 }
 
-pub fn choreo_videos() -> Vec<VideoType> {
-    vec![
-        VideoType::Regular(Video {
-            id: 1,
-            url: "static/AI&Boy.mp4".to_string(),
-            loop_video: false,
-        }),
-        VideoType::Regular(Video {
-            id: 2,
-            url: "static/Siblings.mp4".to_string(),
-            loop_video: false,
-        }),
-        VideoType::Regular(Video {
-            id: 3,
-            url: "static/Culture4Fun.mp4".to_string(),
-            loop_video: false,
-        }),
-        VideoType::Regular(Video {
-            id: 4,
-            url: "static/fodbold_tiny.mp4".to_string(),
-            loop_video: false,
-        }),
-    ]
+pub async fn choreo_videos(config: &Config) -> Vec<VideoType> {
+    config.load_choreo_videos()
 }
+// pub fn choreo_videos() -> Vec<VideoType> {
+//     vec![
+//         VideoType::Regular(Video {
+//             id: 1,
+//             url: "static/AI&Boy.mp4".to_string(),
+//             loop_video: false,
+//         }),
+//         VideoType::Regular(Video {
+//             id: 2,
+//             url: "static/Siblings.mp4".to_string(),
+//             loop_video: false,
+//         }),
+//         VideoType::Regular(Video {
+//             id: 3,
+//             url: "static/Culture4Fun.mp4".to_string(),
+//             loop_video: false,
+//         }),
+//         VideoType::Regular(Video {
+//             id: 4,
+//             url: "static/fodbold_tiny.mp4".to_string(),
+//             loop_video: false,
+//         }),
+//     ]
+// }
 
 pub fn loadscreen_video() -> Vec<VideoType> {
     vec![
--- src/components/molecules/async_await_choreo_video.rs
+++ src/components/molecules/async_await_choreo_video.rs
@@ -1,25 +1,15 @@
-//src/components/organisms/choreo_videos.rs
-use crate::choreo_videos;
-use crate::components::molecules::video_list::VideosListProps;
-use crate::loadscreen_video;
-use crate::use_location;
-use crate::Route;
-use crate::VideosList;
+//src/components/molecules/async_await_choreo_video.rs
 use yew::prelude::*;
-use yew_router::prelude::{use_navigator, Navigator};
-use crate::components::atoms::use_focus_div::use_focus_div;
-use crate::components::data::config::get_config_path;
+use crate::components::data::video_data::choreo_videos;
 use crate::components::data::config::Config;
+use crate::components::data::config::get_config_path;
+use crate::VideosList;
 
-
-#[function_component(ChoreoVideo)]
-pub fn choreographic_videos() -> Html {
-    // State to hold the videos and loading status
+#[function_component(ChoreoVideosComponent)]
+pub fn choreo_videos_component() -> Html {
     let choreo_videos_state = use_state(|| vec![]);
     let loading = use_state(|| true);
-    let navigator = use_navigator().unwrap();
-    let div_ref = use_focus_div();
-    
+
     // Load videos from config when component mounts
     use_effect_with((), {
         let choreo_videos_state_clone = choreo_videos_state.clone();
@@ -50,28 +40,13 @@ pub fn choreographic_videos() -> Html {
     let choreo_video_index = use_state(|| 0);
     let handle_video_ended = {
         let choreo_video_index = choreo_video_index.clone();
-        Callback::from(move |_: usize| {
+        Callback::from(move |_| {
             let new_index = *choreo_video_index + 1;
             choreo_video_index.set(new_index);
         })
     };
 
-    // Callback for handling the ended event of the video element
-    let handle_video_ended = {
-        let navigator = navigator.clone();
-        Callback::from(move |_| {
-            navigator.push(&Route::LoadScreenVideo);
-        })
-    };
-
-    let restart_app = Callback::from(move |event: KeyboardEvent| {
-        if event.key() == "q" {
-            navigator.push(&Route::IntroScreen1);
-        }
-    });
-
     html! {
-    <div ref={div_ref} onkeydown={restart_app} tabindex="0">
         if *loading {
             <div class="about-choreo-container">
                 <p>{"Loading..."}</p>
@@ -84,9 +59,5 @@ pub fn choreographic_videos() -> Html {
                 video_class="fullscreenvideo"
             />
         }
-    </div>
     }
 }
-
-    
-
