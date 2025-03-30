9f6f2f322668caa0a840e851dfefaa994f944be3
martinschultzkristensen <m4rtinsk@protonmail.com>
2025-03-30 22:14:17 +0200

    compiling adding  in tauri.conf.json "withGlobalTauri": true, created trunk.toml file with properties ormalizing configuration.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,8 +2,14 @@
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 
+#[tauri::command]
+fn my_custom_command() {
+  println!("I was invoked from JavaScript!");
+}
+
 pub fn run() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![my_custom_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
