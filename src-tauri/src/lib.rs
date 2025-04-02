e358c47cd425f7192a1bd65d70ec76dd836506b6
martinschultzkristensen <m4rtinsk@protonmail.com>
2025-04-02 16:40:21 +0200

    created config_manager.rs to find dynamically config path after build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,16 @@
 //src-tauri/src/lib.rs
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+mod config_manager;
 
 pub fn run() {
     tauri::Builder::default()
+        // Register the commands
+        .invoke_handler(tauri::generate_handler![
+            config_manager::get_config,
+            config_manager::save_config,
+            config_manager::get_asset_path,
+            config_manager::list_assets,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
