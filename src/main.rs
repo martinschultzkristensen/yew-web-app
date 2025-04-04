b062b8d6ca52b487e5becf2bd51828976f22a197
martinschultzkristensen <m4rtinsk@protonmail.com>
2025-04-04 09:37:56 +0200

    added logging conditions in fn_main()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 //main.rs
 use yew_app::DanceOmatic;
+use cfg_if::cfg_if;
+
+//Environment	Logger	Output
+// Web (Trunk/Yew) --> wasm_logger --> Browser Console
+// Tauri Native	--> env_logger or tauri::log --> Terminal / Dev Tools
 
 fn main() {
-    env_logger::init();
+    cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            wasm_logger::init(wasm_logger::Config::default());
+            console_error_panic_hook::set_once();
+        }
+    else {
+        // For native (Tauri backend, CLI, etc.)
+        env_logger::init();
+    }
+}
     yew::Renderer::<DanceOmatic>::new().render();
 }
