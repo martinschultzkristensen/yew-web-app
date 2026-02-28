81c32bf50b8a938a8a5f55eebcb5e95835c75bc8
martinschultzkristensen <m4rtinsk@protonmail.com>
2026-02-28 07:00:05 +0100

    build app now plays bundled devVideos if no media from 'Library/Application Support/danceOmatic/media'

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -330,34 +330,4 @@ pub fn debug_paths(handle: tauri::AppHandle) -> Result<String, String> {
     ))
 }
 
-//this command plays sound from backend. Works, but makes the sound more laggy.
-// #[tauri::command]
-// fn play_sound_backend(sound_file: String) {
-//     // Resolve absolute path (you can adjust based on how/where you store files)
-//     let base_path = tauri::api::path::resource_dir().expect("No resource dir");
-//     let full_path = base_path.join(sound_file);
 
-//     let (_stream, handle) = OutputStream::try_default().expect("No audio output stream");
-//     let sink = Sink::try_new(&handle).expect("Failed to create audio sink");
-
-//     let file = File::open(full_path).expect("Failed to open sound file");
-//     let source = Decoder::new(BufReader::new(file)).expect("Failed to decode audio");
-
-//     sink.append(source);
-//     sink.detach(); // play-and-forget
-// }
-
-// #[tauri::command]
-// pub fn get_video_path(handle: tauri::AppHandle, video_name: String) -> Result<String, String> {
-//     let start = std::time::Instant::now(); //<--logger variable
-//     let video_path = handle.path()
-//         .resolve(&format!("static/devVideo/{}", video_name), BaseDirectory::Resource)
-//         .map_err(|e| e.to_string())?;
-
-//         log::info!("my_command from tauri backend took {:?}", start.elapsed()); //<--logger shows in terminal
-
-//     video_path.to_str()
-//         .map(|s| s.to_string())
-//         .ok_or("Failed to convert path to string".to_string())
-
-// }
